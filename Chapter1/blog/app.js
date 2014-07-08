08306ea25ce89fa25becd9ab1ef3d615097e74a7
JerroldLee <lijiayouyj@163.com>
2014-07-09 00:43:24 +0800

    before flash

--- Chapter1/blog/app.js
+++ Chapter1/blog/app.js
@@ -7,6 +7,8 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var MongoStore = require('connect-mongo')(express);
+var settings = require('./settings');
 
 var app = express();
 
@@ -19,6 +21,15 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session ({
+  secret: settings.cookieSecret,
+  key: settings.db,
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  store: new MongoStore({
+    db: settings.db
+  })
+}));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
