3ca39e24436d6b72a2b16bbbefa0547e24b5fa3b
JerroldLee <lijiayouyj@163.com>
2014-07-10 22:12:40 +0800

    第二章，为文章增加markdowm功能

--- Chapter1/blog/routes/index.js
+++ Chapter1/blog/routes/index.js
@@ -3,18 +3,27 @@
  * GET home page.
  */
 var crypto = require('crypto'),
-		User = require('../models/user.js');
+		User = require('../models/user.js'),
+		Post = require('../models/post.js');
 
 module.exports = function (app) {
 	app.get('/', function (req, res) {
-		res.render('index', {
-			title: '主页',
-			user: req.session.user,
-			success: req.flash('success').toString(),
-			error: req.flash('error').toString()
+		Post.get(null, function (err, posts) {
+			if (err) {
+				posts = [];
+			}
+			res.render('index', {
+				title: '主页',
+				user: req.session.user,
+				posts: posts,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
 		});
+		
 	});
 
+	app.get('/reg', checkNotLogin);
 	app.get('/reg', function (req, res) {
 		res.render('reg', {
 			title: '注册',
@@ -24,6 +33,7 @@ module.exports = function (app) {
 		});
 	});
 
+	app.post('/reg', checkNotLogin);
 	app.post('/reg', function (req, res) {
 		var name = req.body.name,
 				password = req.body.password,
@@ -63,6 +73,7 @@ module.exports = function (app) {
 		});
 	});
 
+	app.get('/login', checkNotLogin);
 	app.get('/login', function (req, res) {
 		res.render('login', {
 			title: '登录',
@@ -72,6 +83,7 @@ module.exports = function (app) {
 		});
 	});
 
+	app.post('/login', checkNotLogin);
 	app.post('/login', function (req, res) {
 		//生成密码的md5值
 		var md5 = crypto.createHash('md5'),
@@ -94,12 +106,28 @@ module.exports = function (app) {
 		});
 	});
 
-	app.get('post', function (req, res) {
-		res.render('post', {title: '发表'});
+	app.get('/post', checkLogin);
+	app.get('/post', function (req, res) {
+		res.render('post', {
+			title: '发表',
+			user: req.session.user,
+			success: req.flash('success'),
+			error: req.flash('error')
+		});
 	});
 
-	app.post('post', function (req, res) {
-
+	app.post('/post', checkLogin);
+	app.post('/post', function (req, res) {
+		var currentUser = req.session.user,
+				post = new Post(currentUser.name, req.body.title, req.body.post);
+		post.save(function (err) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+			req.flash('success', '发布成功!');
+			res.redirect('/');//发表成功跳转到主页
+		});
 	});
 
 	app.get('/logout', function (req, res) {
@@ -107,6 +135,22 @@ module.exports = function (app) {
 		req.flash('success', '退出成功！');
 		res.redirect('/');//退出成功后跳转到主页
 	});
+
+	function checkLogin(req, res, next) {
+		if (!req.session.user) {
+			req.flash('error', '未登录！');
+			res.redirect('/login');
+		}
+		next();
+	}
+
+	function checkNotLogin(req, res, next) {
+		if (req.session.user) {
+			req.flash('error', '已登录！');
+			res.redirect('back');//返回之前的页面
+		}
+		next();
+	}
 
 
 };
