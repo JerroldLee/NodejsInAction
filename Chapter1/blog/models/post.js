4bb1c9bffa18528101546dc1ce8936f33ec5ea54
Jerrold.Lee <lijiayouyj@163.com>
2014-07-29 12:28:23 +0800

    第9章--增加标签和标签页面

--- Chapter1/blog/models/post.js
+++ Chapter1/blog/models/post.js
@@ -1,9 +1,10 @@
 var mongodb = require('./db'),
     markdown = require('markdown').markdown;
 
-function Post(name, title, post) {
+function Post(name, title, tags, post) {
   this.name = name;
   this.title = title;
+  this.tags = tags;
   this.post = post;
 }
 
@@ -26,6 +27,7 @@ Post.prototype.save = function(callback) {
       name: this.name,
       time: time,
       title: this.title,
+      tags: this.tags,
       post: this.post,
       comments: []
   };
@@ -85,9 +87,9 @@ Post.getTen = function(name, page, callback) {
             return callback(err);
           }
           //解析markdown为html
-          docs.forEach(function (doc) {
+          /*docs.forEach(function (doc) {
             doc.post = markdown.toHTML(doc.post);
-          });
+          });*/
           callback(null, docs, total);
         });
       });
@@ -269,3 +271,57 @@ Post.getOne = function (name, day, title, callback) {
       });
     });
   }
+
+  //返回所有标签
+  Post.getTags = function (callback) {
+    mongodb.open(function (err, db) {
+      if (err) {
+        return callback(err);
+      }
+      db.collection('posts', function (err, collection) {
+        if (err) {
+          mongodb.close();
+          return callback(err);
+        }
+        //distinct 用来找出给定键的所有不同值
+        collection.distinct('tags', function (err, docs) {
+          mongodb.close();
+          if (err) {
+            return callback(err);
+          }
+          callback(null, docs);
+        });
+      });
+    });
+  };
+
+  //返回含有特定标签的所有文章
+  Post.getTag = function (tag, callback) {
+    mongodb.open(function (err, db) {
+      if (err) {
+        return callback(err);
+      } 
+      db.collection('posts', function (err, collection) {
+        if (err) {
+          mongodb.close();
+          return callback(err);
+        }
+        //查询所有tags数组内包含tag组成的数组
+        collection.find({
+          'tags': tag
+        }, {
+          'name': 1,
+          'time': 1,
+          'title': 1
+        }).sort({
+          time: -1
+        }).toArray(function (err, docs) {
+          mongodb.close();
+          if (err) {
+            return callback(err);
+          }
+          callback(null, docs);
+        });
+      });
+    });
+  };
--- Chapter1/blog/routes/index.js
+++ Chapter1/blog/routes/index.js
@@ -17,6 +17,7 @@ module.exports = function (app) {
 			if (err) {
 				posts = [];
 			}
+			console.log(posts);
 			res.render('index', {
 				title: '主页',
 				user: req.session.user,
@@ -127,7 +128,8 @@ module.exports = function (app) {
 	app.post('/post', checkLogin);
 	app.post('/post', function (req, res) {
 		var currentUser = req.session.user,
-				post = new Post(currentUser.name, req.body.title, req.body.post);
+				tags = [req.body.tag1, req.body.tag2, req.body.tag3],
+				post = new Post(currentUser.name, req.body.title, tags, req.body.post);
 		post.save(function (err) {
 			if (err) {
 				req.flash('error', err);
@@ -181,6 +183,38 @@ module.exports = function (app) {
 			}
 			res.render('archive', {
 				title: '存档',
+				posts: posts,
+				user: req.session.user,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
+	});
+
+	app.get('/tags', function (req, res) {
+		Post.getTags(function (err, posts) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+			res.render('tags', {
+				title: '标签',
+				posts: posts,
+				user: req.session.user,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
+	});
+
+	app.get('/tags/:tag', function (req, res) {
+		Post.getTag(req.params.tag, function (err, posts) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+			res.render('tag', {
+				title: 'Tag: ' + req.params.tag,
 				posts: posts,
 				user: req.session.user,
 				success: req.flash('success').toString(),
